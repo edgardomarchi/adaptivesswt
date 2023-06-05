23ef9e1d00a8d1da1e1d63a5c06996e5fa859acb
Edgardo Marchi <edgardo.marchi@gmail.com>
2023-06-05 11:27:12 -0300

    Bump version: 0.2.1 → 0.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Edgardo Marchi'
 author = 'Edgardo Marchi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
